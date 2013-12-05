568dc59e6b49d8575d5c38f76e50e2f2b51ecf32
grosser <michael@grosser.it>
2013-12-04 17:12:51 -0800

    avoid duplicate comments in mysql

--- lib/active_record/comments.rb
+++ lib/active_record/comments.rb
@@ -6,21 +6,20 @@ module ActiveRecord
       class << self
         def included(base)
           base.class_eval do
-            # 99% case
-            if base.method_defined?(:execute)
-              alias_method :execute_without_comment, :execute
-              def execute(query, *args, &block)
-                query = ActiveRecord::Comments.with_comment_sql(query)
-                execute_without_comment(query, *args, &block)
-              end
-            end
-
             # ActiveRecord 3.2 vs sqlite, maybe others ...
             if base.method_defined?(:exec_query)
               alias_method :exec_query_without_comment, :exec_query
               def exec_query(query, *args, &block)
                 query = ActiveRecord::Comments.with_comment_sql(query)
                 exec_query_without_comment(query, *args, &block)
+              end
+
+            # 99% case
+            elsif base.method_defined?(:execute)
+              alias_method :execute_without_comment, :execute
+              def execute(query, *args, &block)
+                query = ActiveRecord::Comments.with_comment_sql(query)
+                execute_without_comment(query, *args, &block)
               end
             end
           end
