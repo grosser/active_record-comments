94f553bf16b12b6667aadf3087be54b51b7ada2a
Nony Dutton <nony.dutton@zendesk.com>
2024-01-15 10:54:55 +0100

    Add support for Rails 7.1
    Raises the `activerecord` upper limit to `< 7.2`.
    
    This also:
    1) Starts patching `#internal_exec_query` if that method exists. `exec_query` was renamed to `internal_exec_query` in each adapter's
    `database_statements` in Rails 7.1:
    https://github.com/rails/rails/commit/942785fb8b03772dde338cc241af8e5de4d2efbc?diff=unified&w=0.
    The abstract `exec_query` [calls
    internal_exec_query](https://github.com/rails/rails/blob/942785fb8b03772dde338cc241af8e5de4d2efbc/activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb#L141)
    so I think we only need to patch `internal_exec_query`.
    
    2) Added a kwarg splat to `exec_query`. Rails introduced an [async:
    false](https://github.com/rails/rails/commit/7fc174aadaefc2c0a8a6b7c8a7599dd9ca04811f)
    kwarg to `exec_query` which this gem wasn't respecting.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,9 +24,18 @@ LOG = []
 
 ActiveRecord::ConnectionAdapters::SQLite3Adapter.class_eval do
   alias_method :exec_query_without_log, :exec_query
-  def exec_query(query, *args, prepare: false, &block)
+  def exec_query(query, *args, **kwargs,  &block)
     LOG << query
-    exec_query_without_log(query, *args, prepare: prepare, &block)
+    exec_query_without_log(query, *args, **kwargs, &block)
+  end
+
+  # Rails 7.1.
+  if ActiveRecord::ConnectionAdapters::SQLite3Adapter.method_defined?(:internal_exec_query)
+    alias_method :internal_exec_query_without_log, :internal_exec_query
+    def internal_exec_query(query, *args, **kwargs, &block)
+      LOG << query
+      internal_exec_query_without_log(query, *args, **kwargs, &block)
+    end
   end
 end
 
